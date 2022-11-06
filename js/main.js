0f7b683102bf0e53cfa00f11affafc17bfc7a32d
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2022-11-06 11:47:07 +0100

    js refactoring

--- js/main.js
+++ js/main.js
@@ -9,19 +9,50 @@ let activeLine = null;
 
 const API_URL = 'http://composition.jarivanmelckebeke.be';
 
-const handleFormSearchSubmit = function (event) {
-    event.preventDefault();
-    const lineSearchValue = lineSearchElement.value;
-    loadLine(lineSearchValue);
+//#region utils
+
+const normalizeStop = function (stop) {
+    let stopName = stop;
+    if (!stop) {
+        console.log('No stop name');
+        return '';
+    }
+    if (stop.includes('/')) {
+        // set stopName to the second part of the string (e.g. the Dutch name)
+        stopName = stopName.split('/')[1];
+    }
+    return stopName;
 }
 
-const htmlGenerateLineHeader = function (data) {
-    return `
-        <h1 class="c-line-title">${data.identifier}</h1>
-    `;
+const checkShouldShowTrip = function (trip) {
+    const activeDays = trip.days.map(day => day.toLowerCase());
+    if (activeDays.indexOf(daySelectElement.value) === -1) {
+        return false;
+    }
+    return true;
 }
 
-const generateAccuracies = function () {
+//#endregion
+
+//#region html generation
+const htmlShowLine = function (data) {
+    let tripHtml = "";
+    for (const trip of data.trips) {
+        if (checkShouldShowTrip(trip)) {
+            tripHtml += htmlGenerateTrip(trip);
+        }
+    }
+    lineSearchResultsElement.innerHTML = `
+    <div class="c-result">
+        <div class="c-result__header">
+            ${htmlGenerateLineHeader(data)}
+        </div>
+        ${tripHtml}
+    </div>`;
+    htmlGenerateAccuracyCharts();
+}
+
+const htmlGenerateAccuracyCharts = function () {
     const containers = document.querySelectorAll('.js-accuracy');
     for (const container of containers) {
         const target = container.querySelector('.js-gauge');
@@ -29,85 +60,76 @@ const generateAccuracies = function () {
         let accuracy = target.dataset.accuracy;
         accuracy = Math.round(accuracy / 5 * 100);
         let opts = {
-            angle: 0, // The span of the gauge arc
-            lineWidth: 0.1, // The line thickness
-            radiusScale: 1, // Relative radius
+            angle: 0,                   // The span of the gauge arc
+            lineWidth: 0.1,             // The line thickness
+            radiusScale: 1,             // Relative radius
             pointer: {
-                length: 0, // // Relative to gauge radius
-                strokeWidth: 0, // The thickness
-                color: '#000000' // Fill color
+                length: 0,              // Relative to gauge radius
+                strokeWidth: 0,         // The thickness
+                color: '#000000'        // Fill color
             },
-            limitMax: false,     // If false, max value increases automatically if value > maxValue
-            limitMin: false,     // If true, the min value of the gauge will be fixed
-            colorStart: '#58FCAD',   // original: #6F6EA0
-            colorStop: '#32de8a',    // original: #C0C0DB
-            strokeColor: '#eee',  // original: #EEEEEE
+            limitMax: false,            // If false, max value increases automatically if value > maxValue
+            limitMin: false,            // If true, the min value of the gauge will be fixed
+            colorStart: '#58FCAD',      // original: #6F6EA0
+            colorStop: '#32de8a',       // original: #C0C0DB
+            strokeColor: '#eee',        // original: #EEEEEE
             generateGradient: true,
             highDpiSupport: true
-
         };
-        const gauge = new Donut(target).setOptions(opts); // create sexy gauge!
+        const gauge = new Donut(target).setOptions(opts);
         gauge.setTextField(textTarget);
         gauge.maxValue = 100;
-        gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-        gauge.animationSpeed = 80; // set animation speed (32 is default value)
-        gauge.set(accuracy); // set actual value
+        gauge.setMinValue(0);
+        gauge.animationSpeed = 32;
+        gauge.set(accuracy);
     }
 
 
 }
 
-const htmlGenerateTrip = function (trip) {
-    // first check if this trip should be shown
-    const activeDays = trip.days.map(day => day.toLowerCase());
-    if (activeDays.indexOf(daySelectElement.value) === -1) {
-        return '';
-    }
-    const htmlGenerateComposition = function (composition) {
-        const htmlGenerateWagon = function (wagon) {
-            return `
-                <div class="c-wagons-table__item">
-                    <img class="c-wagon__image" src="${API_URL}${wagon.img}" alt="${wagon.identifier} produced by ${wagon.company}">
-                    <span class="c-wagon__from">${wagon.endpoints.start}</span>
-                    <span class="c-wagon__to">${wagon.endpoints.end}</span>
-                </div>`;
-        }
+const htmlGenerateLineHeader = function (data) {
+    return `<h1 class="c-line-title">${data.identifier}</h1>`;
+}
 
-        return `
-            <div class="c-trip__composition">
-                <h2 class="c-trip__active-condition">${composition.condition}</h2>
-                    <div class="c-wagons-table">
-                        ${composition.composition.map(htmlGenerateWagon).join('')}
-                    </div>
+const htmlGenerateWagon = function (wagon) {
+    return `<div class="c-wagons-table__item">
+                <img class="c-wagon__image" src="${API_URL}${wagon.img}" 
+                                            alt="${wagon.identifier} produced by ${wagon.company}">
+                <span class="c-wagon__from">${wagon.endpoints.start}</span>
+                <span class="c-wagon__to">${wagon.endpoints.end}</span>
             </div>`;
-    }
-    const htmlGenerateStop = function (stop) {
-        let stopName = stop;
-        if (!stop) {
-            console.log('No stop name');
-            return '';
-        }
-        if (stop.includes('/')) {
-            // set stopName to the second part of the string
-            stopName = stopName.split('/')[1];
-        }
+}
+
+const htmlGenerateComposition = function (composition) {
+    return `<div class="c-trip__composition">
+                <h2 class="c-trip__active-condition">${composition.condition}</h2>
+                <div class="c-wagons-table">
+                    ${composition.composition.map(htmlGenerateWagon).join('')}
+                </div>
+            </div>`;
+}
+const htmlGenerateStop = function (stop) {
+    let stopName = normalizeStop(stop);
+    if (stopName)
         return `<li class="c-line-stops__item">${stopName}</li>`
-    }
+    return '';
+}
 
 
+const htmlGenerateTrip = function (trip) {
     const amountOfStops = trip.stops.length;
-    const inbetweenStopsText = `${amountOfStops - 2} stops`;
-    let inbetweenHtml = "";
+    let stopHtml = htmlGenerateStop(trip.stops[0]);
     if (amountOfStops > 2) {
-        inbetweenHtml = htmlGenerateStop(inbetweenStopsText);
+        const inbetweenStopsText = `${amountOfStops - 2} stops`;
+        stopHtml += htmlGenerateStop(inbetweenStopsText);
     }
-    const result = `
+    stopHtml += htmlGenerateStop(trip.stops[amountOfStops - 1]);
+
+    return `
     <div class="c-trip">
         <div class="c-trip__header">
             <ul class="c-line-stops">
-                ${htmlGenerateStop(trip.stops[0])}
-                ${inbetweenHtml}
-                ${htmlGenerateStop(trip.stops[trip.stops.length - 1])}
+                ${stopHtml}
             </ul>
             <div class="c-trip__accuracy js-accuracy">
                 <h2 class="c-trip__accuracy-title">Accuracy</h2>
@@ -119,21 +141,10 @@ const htmlGenerateTrip = function (trip) {
             ${trip.compositions.map(htmlGenerateComposition).join('')}
         </div>
     </div>`;
-    return result;
 }
+//#endregion
 
-const ensureClassActive = function (querySelector, cls) {
-    if (!document.querySelector(querySelector).classList.contains(cls)) {
-        document.querySelector(querySelector).classList.add(cls);
-    }
-}
-
-const ensureClassInactive = function (querySelector, cls) {
-    if (document.querySelector(querySelector).classList.contains(cls)) {
-        document.querySelector(querySelector).classList.remove(cls);
-    }
-}
-
+//#region css tools
 const activateContainer = function (querySelector) {
     const element = document.querySelector(querySelector);
     if (!element) {
@@ -167,7 +178,6 @@ const deactivateContainer = function (querySelector) {
 const activateResults = function () {
     activateContainer('.js-search-results');
     activateContainer('.js-day');
-    activateContainer('.js-header');
     activateContainer('.js-page');
 }
 
@@ -175,21 +185,14 @@ const hideResults = function () {
     deactivateContainer('.js-search-results');
     // deactivateContainer('.js-day');
     // don't deactivate day, because it's not needed
-    deactivateContainer('.js-header');
     deactivateContainer('.js-page');
 }
+//#endregion
 
-const htmlShowLine = function (data) {
-    const result = `
-    <div class="c-result">
-        <div class="c-result__header">
-            ${htmlGenerateLineHeader(data)}
-        </div>
-        ${data.trips.map(htmlGenerateTrip).join('')}
-    </div>`;
-    lineSearchResultsElement.innerHTML = result;
-    generateAccuracies();
-
+const handleFormSearchSubmit = function (event) {
+    event.preventDefault();
+    const lineSearchValue = lineSearchElement.value;
+    loadLine(lineSearchValue);
 }
 
 const loadLine = function (line) {
